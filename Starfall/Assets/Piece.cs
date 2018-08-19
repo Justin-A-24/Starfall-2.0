d6d4f177736e38212d8f7fd081347c0c22117e54
Juan Alvarez <juanalvarez391@yahoo.com>
2018-08-18 22:55:08 -0700

    On each play through, the background will be chosen randomly

--- Starfall/Assets/Piece.cs
+++ Starfall/Assets/Piece.cs
@@ -2,8 +2,9 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class LevelManager : MonoBehaviour {
+public class Piece : MonoBehaviour {
     public GameObject endless_piece;
+    public int bg_index;
 
     [SerializeField]
     private bool canSpawn;
@@ -11,30 +12,30 @@ public class LevelManager : MonoBehaviour {
     [SerializeField]
     private GameObject[] background;
 
-    //[SerializeField]
-    public int bg_index;
-
-	// Use this for initialization
-	void Start () {
+    public void Start()
+    {
         canSpawn = true;
-
-        // Chooses a random background and sets it to active
-        bg_index = Random.Range(0, background.Length);
-        background[bg_index].SetActive(true);
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         // Checks to see if the player has cross the collider in order to 
         // spawn a new piece
-        if (collision.CompareTag("Player") && canSpawn) {
+        if (collision.CompareTag("Player") && canSpawn)
+        {
             canSpawn = false;
             Spawn();
         }
     }
 
+    public void ChangeBG()
+    {
+        background[bg_index].SetActive(true);
+    }
+
     // Spawns a new piece to the endless level
-    public void Spawn() {
+    public void Spawn()
+    {
         var piece = Instantiate(endless_piece, transform.position, Quaternion.identity);
         Piece spawned_piece = piece.GetComponentInChildren<Piece>();
         spawned_piece.bg_index = bg_index;
